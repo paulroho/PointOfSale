192b3b85c9ef9966b267adc435cc2ccfa5a9493c
Paul Rohorzka <paul.rohorzka@techtalk.at>
2018-10-04 15:08:10 +0200

    Testing a non-registered item

--- PointOfSale/CashRegisterTests.cs
+++ PointOfSale/CashRegisterTests.cs
@@ -24,5 +24,21 @@ namespace PointOfSale
 					.WithArgs<ProductEventArgs>(e => e.Price == 123.45m);
 			}
 		}
+
+		[Fact]
+		public void DoesntSellNonRegisteredItem()
+		{
+			var cashRegister = new CashRegister();
+
+			using (var monitoredCashRegister = cashRegister.Monitor())
+			{
+				// Act
+				cashRegister.Scan("anotherbarcode");
+
+				// Assert
+				monitoredCashRegister.Should()
+					.NotRaise(nameof(CashRegister.ProductSuccessfullyScanned));
+			}
+		}
 	}
 }
